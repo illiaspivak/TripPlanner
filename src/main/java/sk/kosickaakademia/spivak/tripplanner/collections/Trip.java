63210387899f824ba9cde9ffc63e8b7261dfc509
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-16 10:32:07 +0200

    add constructor and getter in class Trip

--- src/main/java/sk/kosickaakademia/spivak/tripplanner/collections/Trip.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/collections/Trip.java
@@ -1,4 +1,37 @@
 package sk.kosickaakademia.spivak.tripplanner.collections;
 
 public class Trip {
+    private String title;
+    private String place;
+    private int approximateDistance;
+    private boolean placeVisited;
+    private int difficultyLevel;
+
+    public Trip(String title, String place, int approximateDistance, boolean placeVisited, int difficultyLevel) {
+        this.title = title;
+        this.place = place;
+        this.approximateDistance = approximateDistance;
+        this.placeVisited = placeVisited;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public int getApproximateDistance() {
+        return approximateDistance;
+    }
+
+    public boolean isPlaceVisited() {
+        return placeVisited;
+    }
+
+    public int getDifficultyLevel() {
+        return difficultyLevel;
+    }
 }
