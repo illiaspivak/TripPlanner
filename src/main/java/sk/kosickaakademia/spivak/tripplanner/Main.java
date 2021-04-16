f0fc1b05139d133274a852073f669655dffa7f23
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-16 11:01:34 +0200

    method test - Testing the database connection

--- src/main/java/sk/kosickaakademia/spivak/tripplanner/Main.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/Main.java
@@ -1,9 +1,12 @@
 package sk.kosickaakademia.spivak.tripplanner;
 
+import sk.kosickaakademia.spivak.tripplanner.database.MongoDB;
+
 public class Main
 {
     public static void main( String[] args )
     {
-
+        MongoDB mongoDB = new MongoDB();
+        mongoDB.test();
     }
 }
--- src/main/java/sk/kosickaakademia/spivak/tripplanner/database/MongoDB.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/database/MongoDB.java
@@ -1,4 +1,24 @@
 package sk.kosickaakademia.spivak.tripplanner.database;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import sk.kosickaakademia.spivak.tripplanner.log.Log;
+
 public class MongoDB {
+    Log log = new Log();
+    private static MongoClient client = MongoClients.create();
+
+    /**
+     * Testing the database connection
+     */
+    public void test() {
+        MongoDatabase database = client.getDatabase("testMongo");
+        MongoCollection<Document> collection = database.getCollection("collection");
+        log.okay("There is a connection to the database");
+        Document document = new Document("name", "test").append("number",1);
+        collection.insertOne(document);
+    }
 }
