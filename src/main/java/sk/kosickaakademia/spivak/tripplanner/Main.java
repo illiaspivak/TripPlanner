d6bffd16294d7758f8b5ef4a4f3a18595a5af479
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-19 10:36:29 +0200

    method insertTrip - Adding a trip to the database

--- src/main/java/sk/kosickaakademia/spivak/tripplanner/Main.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/Main.java
@@ -1,5 +1,6 @@
 package sk.kosickaakademia.spivak.tripplanner;
 
+import sk.kosickaakademia.spivak.tripplanner.collections.Trip;
 import sk.kosickaakademia.spivak.tripplanner.database.MongoDB;
 
 public class Main
@@ -7,6 +8,7 @@ public class Main
     public static void main( String[] args )
     {
         MongoDB mongoDB = new MongoDB();
-        mongoDB.test();
+        Trip trip = new Trip("Ďumbier", "Nízké Tatry", 30,false, 3);
+        mongoDB.insertTrip(trip);
     }
 }
--- src/main/java/sk/kosickaakademia/spivak/tripplanner/database/MongoDB.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/database/MongoDB.java
@@ -5,20 +5,29 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import sk.kosickaakademia.spivak.tripplanner.collections.Trip;
 import sk.kosickaakademia.spivak.tripplanner.log.Log;
 
 public class MongoDB {
     Log log = new Log();
     private static MongoClient client = MongoClients.create();
 
+
     /**
-     * Testing the database connection
+     * Adding a trip to the database
+     * @param trip
      */
-    public void test() {
-        MongoDatabase database = client.getDatabase("testMongo");
-        MongoCollection<Document> collection = database.getCollection("collection");
-        log.okay("There is a connection to the database");
-        Document document = new Document("name", "test").append("number",1);
-        collection.insertOne(document);
+    public void insertTrip(Trip trip) {
+        try{
+            MongoDatabase database = client.getDatabase("tripplanner");
+            MongoCollection<Document> collection = database.getCollection("trips");
+            Document document = new Document("title",trip.getTitle()).append("place",trip.getPlace()).append("distance", trip.getApproximateDistance()).append("placeVisited",trip.isPlaceVisited()).append("difficultyLevel",trip.getDifficultyLevel());
+            collection.insertOne(document);
+            log.okay("Trip added");
+        }catch (Exception e){
+            log.error(e.toString());
+        }
+
     }
+
 }
