f2c76a52b153cf500b4d6acb7af7a12efc84ddc8
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-04-25 21:46:40 +0200

    add method GET: Show all trips

--- src/main/java/sk/kosickaakademia/spivak/tripplanner/controller/Controller.java
+++ src/main/java/sk/kosickaakademia/spivak/tripplanner/controller/Controller.java
@@ -1,4 +1,21 @@
 package sk.kosickaakademia.spivak.tripplanner.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sk.kosickaakademia.spivak.tripplanner.database.MongoDB;
+
+@RestController
 public class Controller {
+
+    /**
+     * Method GET: Show all trips
+     * @return json
+     */
+    @GetMapping("/trip")
+    public ResponseEntity<String> getAllUsers(){
+        String json = new MongoDB().getAllTripsJson();
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(json);
+    }
 }
